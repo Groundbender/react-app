0d19bee334acdb339b45e75f0512a3af7aac1afb
Damir <damir24may@mail.ru>
2023-06-14 02:11:27 +0300

    added auth page

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -4,13 +4,15 @@ import { IoMdArrowBack } from 'react-icons/io'
 import { SlUser } from 'react-icons/sl'
 import { useLocation, useNavigate } from 'react-router-dom'
 
+import { useAuth } from '../../../hooks/useAuth'
+
 import Hamburger from '../hamburger/Hamburger'
 
 import styles from './Header.module.scss'
 
 const Header = ({ backLink }) => {
 	// react router
-	// const { isAuth } = useAuth();
+	const { isAuth } = useAuth()
 
 	const { pathname } = useLocation()
 	const navigate = useNavigate()
@@ -22,7 +24,7 @@ const Header = ({ backLink }) => {
 					<IoMdArrowBack fill='#fff' fontSize='29px' />
 				</button>
 			) : (
-				<button onClick={() => navigate('/profile')}>
+				<button onClick={() => navigate(isAuth ? '/profile' : '/auth')}>
 					<SlUser fill='#fff' fontSize='25px' />
 				</button>
 			)}
--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,5 +1,66 @@
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+import Button from '../../ui/button/Button'
+import Field from '../../ui/field/Field'
+import Loader from '../../ui/loader/Loader'
+
+import Layout from '../../layout/Layout'
+
+import styles from './Auth.module.scss'
+
+const isLoading = false
+const isLoadingAuth = false
+
 const Auth = () => {
-	return <div>Auth</div>
+	// eslint-disable-next-line no-unused-vars
+	const [type, setType] = useState('auth')
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		mode: 'onChange'
+	})
+
+	const onSubmit = data => {
+		// type
+		console.log(data)
+	}
+	return (
+		<>
+			<Layout heading='Sign in' bgImage='/public/images/authbg.jpg' />
+			<div className='wrapper-inner-page'>
+				{(isLoading || isLoadingAuth) && <Loader />}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Field
+						error={errors?.email?.message}
+						name='email'
+						register={register}
+						options={{
+							required: 'Email is required'
+						}}
+						type='text'
+						placeholder='Email'
+					/>
+					<Field
+						error={errors?.password?.message}
+						name='password'
+						register={register}
+						options={{
+							required: 'Password is required'
+						}}
+						type='text'
+						placeholder='Password'
+					/>
+					<div className={styles.wrapperButtons}>
+						<Button clickHandler={() => setType('auth')}>Sign in</Button>
+						<Button clickHandler={() => setType('reg')}>Sign up</Button>
+					</div>
+				</form>
+			</div>
+		</>
+	)
 }
 
 export default Auth
